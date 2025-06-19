c94650e120cd9b7214ec07ff3b421c70ecbcedbd
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2025-06-19 15:55:15 -0600

    clases openia

---
+++ BitacoraModels/RespuestaOpenIA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitacoraModels
+{
+    public class RespuestaOpenIA
+    {
+        public string? Respuesta { get; set; }
+        public int TokensEntrada { get; set; }
+        public int TokensSalida { get; set; }
+        public float[]? PreguntaVector { get; set; }
+    }
+}
