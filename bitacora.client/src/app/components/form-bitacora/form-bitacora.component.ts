651e40900a2964519c8e7de57af2072f79ec6645
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2025-06-25 15:25:24 -0600

    ajuste de carga de historial

--- bitacora.client/src/app/components/form-bitacora/form-bitacora.component.ts
+++ bitacora.client/src/app/components/form-bitacora/form-bitacora.component.ts
@@ -363,10 +363,11 @@ export class FormBitacoraComponent implements OnInit, OnDestroy {
       this.fechaSel=true;
     }
   }
-  proyectoSeleccion(event : Event){
+  proyectoSeleccion(event : number){
 
-    
-    var index = this.proyectos.indexOf(this.proyectos.find(x => x.id == event));
+    const proyecto = this.proyectos.find(x => x?.id == event);
+
+    var index = this.proyectos.indexOf(proyecto);
     if(index!=-1){
       this.titleProy = this.proyectos[index].nombre
     }
@@ -376,7 +377,17 @@ export class FormBitacoraComponent implements OnInit, OnDestroy {
     //const actividad = this.formBitacora.get('actividad');
     const evento = this.eventoSeleccionado;
     const proyectoText=this.formBitacora.get('proyectoText');
-    if(Number(event)>0){
+    if (proyecto) {
+      this.titleProy = proyecto.nombre;
+      this.proyectoSel = true;
+      this.mostrarFase = true;
+      this.eventoSeleccionado = undefined;
+      proyectoText!.setValidators(null);
+      console.log("ID del proyecto seleccionado:", proyecto.id);
+      this.filtrarEtapas(proyecto.id);
+      this.filtrarActividades(proyecto.id);
+  
+   /* if(Number(event)>0){
       this.proyectoSel=true;
       this.mostrarFase = true;
       //this.formBitacora.get('fase').enable();
@@ -389,9 +400,10 @@ export class FormBitacoraComponent implements OnInit, OnDestroy {
       //actividad.setValidators([Validators.required]);
       //fase.setValidators([Validators.required]);
       proyectoText!.setValidators(null);
-
+      console.log(this.proyectos[index].id);
+      console.log("index" + index);
       this.filtrarEtapas(this.proyectos[index].id);
-      this.filtrarActividades(this.proyectos[index].id);
+      this.filtrarActividades(this.proyectos[index].id);*/
 
     }else{
       this.proyectoSeleccionado = undefined;
